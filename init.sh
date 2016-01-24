e39f967e8f226ca7ec24e7e4fd74449b8ab8b9b0
Eugen Cristea <eugen@cristea.org>
2016-01-24 16:25:48 +0100

    update

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# git clone git@github.com:tecris/dev-linux.git
+
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+# sudo su - 
+# echo -e '[local]\nlocalhost\n' > /etc/ansible/hosts
+# cat /etc/ansible/hosts 
+# sudo ansible-playbook playbook.yml -c local
