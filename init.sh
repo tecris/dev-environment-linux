b34549ea6e4c20eb95ef1195bcac2d30efd9b704
Eugen Cristea <eugen@cristea.org>
2016-02-07 10:26:10 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+# wget https://github.com/tecris/dev-linux/archive/master.zip
+
+
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
@@ -8,13 +11,4 @@ sudo apt-get install -y ansible
 
 ansible-galaxy install williamyeh.oracle-java
 
-# wget https://github.com/tecris/dev-linux/archive/master.zip
-
 cd ansible && sudo ansible-playbook -i hosts playbook.yml -c local --skip-tags "only-on-docker"
-
-#
-# or,
-# sudo su - 
-# echo -e '[local]\nlocalhost\n' >> /etc/ansible/hosts
-# cat /etc/ansible/hosts 
-# sudo ansible-playbook playbook.yml -c local
