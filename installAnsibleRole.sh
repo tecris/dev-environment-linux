db80f2f8ef83b549095601bf58f390eef99b0ab8
Eugen Cristea <eugen@cristea.org>
2016-02-08 10:50:17 +0100

    added to repo

---
+++ installAnsibleRole.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ansible-galaxy install \
+        williamyeh.oracle-java \
+        angstwad.docker_ubuntu \
+        geerlingguy.git \
+        tecris.maven \
+        tersmitten.screen
