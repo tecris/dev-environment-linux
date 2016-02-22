e85d1d1817b994262313ad488c116176a5f00104
Eugen Cristea <eugen@cristea.org>
2016-02-22 11:30:56 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
 # wget https://github.com/tecris/dev-linux/archive/master.zip
-
 
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
-./installAnsibleRole.sh
+sudo ./installAnsibleRole.sh
 
 cd ansible && sudo ansible-playbook -i hosts playbook.yml -c local --skip-tags "only-on-docker"
