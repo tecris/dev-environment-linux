764b061ff783d41c2361c47f6bb30c4ff5f7b388
Eugen Cristea <eugen@cristea.org>
2016-02-26 11:43:41 +0100

    removed

--- installAnsibleRole.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ansible-galaxy install \
-        williamyeh.oracle-java \
-        williamyeh.nodejs \
-        angstwad.docker_ubuntu \
-        geerlingguy.git \
-        tecris.maven \
-        tersmitten.screen
