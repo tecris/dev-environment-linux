78d50862cce4ccc4b510a283e9769e33e209fd4a
Eugen Cristea <eugen@cristea.org>
2016-08-14 19:40:01 +0300

    Update bareUbuntu.sh

--- bareUbuntu.sh
+++ bareUbuntu.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# wget https://github.com/tecris/dev-linux/archive/master.zip
-
-# sudo apt-get install -y software-properties-common
-# sudo apt-add-repository ppa:ansible/ansible
-# sudo apt-get update
-#sudo apt-get install -y ansible
-
-# ubuntu16.04 (xenial)
-sudo apt-get install -y ansible=2.0.0.2-2
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+# sudo apt-get install -y ansible=2.0.0.2-2
 
 sudo ansible-galaxy install -r requirements.yml
 
