274b89a2055076bb839b92952698e59ee2471ec9
Eugen Cristea <eugen.c@mobfox.com>
2017-07-15 22:11:03 +0200

    cleanup

--- base/bareUbuntu.sh
+++ base/bareUbuntu.sh
@@ -4,8 +4,7 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
-# sudo apt-get install -y ansible=2.0.0.2-2
 
 sudo ansible-galaxy install -r requirements.yml
 
-cd ansible && sudo ansible-playbook -i hosts playbook.yml -c local --skip-tags "only-on-docker"
+sudo ansible-playbook -i ansible/hosts ansible/playbook.yml -c local --skip-tags "only-on-docker"
